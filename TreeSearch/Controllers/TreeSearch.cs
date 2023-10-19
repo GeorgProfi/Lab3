36548f5c8f52edc31384376521a0a9b5d4d69521
gg <gg@weareway.com>
2023-10-19 23:49:11 +0300

    add SearchRandomEl (get request)

--- TreeSearch/Controllers/TreeSearch.cs
+++ TreeSearch/Controllers/TreeSearch.cs
@@ -2,19 +2,39 @@ using Microsoft.AspNetCore.Mvc;
 using TreeSearch.Models;
 using LabsShareLibrary;
 using static LabsShareLibrary.Lab2;
+using System.Security.Cryptography;
 
 namespace TreeSearch.Controllers
 {
     [ApiController]
     public class TreeSearch : ControllerBase
     {
-        [Route("Show/[controller]")]
+        [Route("SearchRandomEl/[controller]")]
         [HttpGet]
         public IActionResult Show()
         {
-            Result result = new Result();
-            result.result = true;
-            return Ok(result);
+            int[] test2 = new int[10];
+            Random randNum = new Random();
+
+            for (int i = 0; i < test2.Length; i++)
+            {
+                test2[i] = randNum.Next(1, 50);
+            }
+
+            RandomInput mas = new RandomInput();
+            mas.input = test2;
+ 
+            Tree tree = new Tree();
+            for (int i = 0; i < mas.input.Length; i++)
+            {
+                tree.Insert(mas.input[i]);
+            }
+
+            int el = randNum.Next(1, 50);
+            mas.searchEl = el;
+            mas.result = tree.Search(el);
+
+            return Ok(mas);
         }
     
     [Route("SearchElement/[controller]")]
---
+++ TreeSearch/Models/RandomInput.cs
@@ -0,0 +1,9 @@
+namespace TreeSearch.Models
+{
+    public class RandomInput
+    {
+        public int[] input { get; set; }
+        public int searchEl { get; set; }
+        public bool result { get; set; }
+    }
+}
